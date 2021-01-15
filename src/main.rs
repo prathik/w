fa6fb7a328ade294e2e3e80701207a3db91f0f6d
Prathik Rajendran M <prathik011@gmail.com>
2021-01-15 14:57:17 +0530

    Enable data collection from given start time

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,11 @@ use std::io::prelude::*;
 use std::io;
 
 fn main() {
-    print!("Enter time: ");
-    io::stdout()
-        .flush()
-        .map_err(|err| println!("{:?}", err))
-        .ok();
-    let mut time = String::new();
-    io::stdin()
-        .read_line(&mut time)
-        .expect("error: unable to read user input");
-    time.pop();
-
-    print!("Enter activity: ");
-    io::stdout()
-        .flush()
-        .map_err(|err| println!("{:?}", err))
-        .ok();
-
-    let mut activity = String::new();
-    io::stdin()
-        .read_line(&mut activity)
-        .expect("error: unable to read user input");
-    activity.pop();
-
-    print!("Enter joy: ");
-    io::stdout()
-        .flush()
-        .map_err(|err| println!("{:?}", err))
-        .ok();
-    let mut joy = String::new();
-    io::stdin()
-        .read_line(&mut joy)
-        .expect("error: unable to read user input");
-    joy.pop();
-
-    print!("Enter importance: ");
-    io::stdout().flush().expect("unable to flush");
-    let mut importance = String::new();
-    io::stdin()
-        .read_line(&mut importance)
-        .expect("error: unable to read user input");
-    importance.pop();
+    let start_hour: u32 = std::env::args()
+        .nth(1)
+        .expect("hour not given")
+        .parse()
+        .unwrap();
 
     let mut file = OpenOptions::new()
         .create(true)
@@ -53,15 +17,49 @@ fn main() {
         .open("w.csv")
         .unwrap();
 
-    if let Err(e) = writeln!(
-        file,
-        "{} {},{},{},{}",
-        chrono::Local::now().format("%Y-%m-%d"),
-        time,
-        activity,
-        joy,
-        importance
-    ) {
-        eprintln!("Couldn't write to file: {}", e);
+    let date = chrono::Local::today();
+    for h in start_hour..start_hour + 3 {
+        let activity_hour = date.and_hms(h, 0, 0);
+        print!("Enter activity at {}: ", activity_hour.format("%H:%M"));
+        io::stdout()
+            .flush()
+            .map_err(|err| println!("{:?}", err))
+            .ok();
+
+        let mut activity = String::new();
+        io::stdin()
+            .read_line(&mut activity)
+            .expect("error: unable to read user input");
+        activity.pop();
+        print!("Enter joy: ");
+
+        io::stdout()
+            .flush()
+            .map_err(|err| println!("{:?}", err))
+            .ok();
+        let mut joy = String::new();
+        io::stdin()
+            .read_line(&mut joy)
+            .expect("error: unable to read user input");
+        joy.pop();
+
+        print!("Enter importance: ");
+        io::stdout().flush().expect("unable to flush");
+        let mut importance = String::new();
+        io::stdin()
+            .read_line(&mut importance)
+            .expect("error: unable to read user input");
+        importance.pop();
+
+        if let Err(e) = writeln!(
+            file,
+            "{},{},{},{}",
+            activity_hour.to_rfc3339(),
+            activity,
+            joy,
+            importance
+        ) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
     }
 }
